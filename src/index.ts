e7174fa1f9122aaf3898b5c6ce24ca81d9e94695
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-04-19 19:35:56 +0400

    chore: rename file to reflect its purpose

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
-import rpc from './rpc';
+import api from './api';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({ maxAge: 86400 }));
 app.use(compression());
-app.use('/', rpc);
+app.use('/', api);
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
