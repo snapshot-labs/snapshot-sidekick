fff3ae7f315825dce00c1b247bf8cb26fcc58d97
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-04-19 18:43:49 +0400

    feat: compress response

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import compression from 'compression';
 import cors from 'cors';
 import rpc from './rpc';
 
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({ maxAge: 86400 }));
+app.use(compression());
 app.use('/', rpc);
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
