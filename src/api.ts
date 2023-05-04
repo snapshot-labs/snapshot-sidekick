447cb91a3cd46377dbc4fe7c34c150b817929eee
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-05-04 12:21:13 +0400

    fix: use await syntax

--- src/api.ts
+++ src/api.ts
@@ -59,16 +59,14 @@ router.post('/votes/:id', async (req, res) => {
       return res.send(Buffer.from(file));
     }
 
-    votesReport
-      .canBeCached()
-      .then(() => {
-        queues.add(id);
-        return rpcError(res, 'PENDING_GENERATION', id);
-      })
-      .catch((e: any) => {
-        log.error(e);
-        rpcError(res, e, id);
-      });
+    try {
+      await votesReport.canBeCached();
+      queues.add(id);
+      return rpcError(res, 'PENDING_GENERATION', id);
+    } catch (e: any) {
+      log.error(e);
+      rpcError(res, e, id);
+    }
   } catch (e) {
     log.error(e);
     return rpcError(res, 'INTERNAL_ERROR', id);
