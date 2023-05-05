b9a09b3d355aeadfc5c8db8ec86b1b77f7f203d3
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-05-06 03:06:39 +0400

    chore: add tests

--- src/lib/moderationList.ts
+++ src/lib/moderationList.ts
@@ -3,19 +3,18 @@ import path from 'path';
 
 const FIELDS = ['flaggedLinks', 'flaggedProposalIds', 'verifiedSpaces'];
 
-export default function moderationList(fields = FIELDS) {
+export function getModerationList(fields = FIELDS) {
   const result: Record<string, string[] | Record<string, number>> = {};
 
   return fields
     .filter(field => FIELDS.includes(field))
-    .map(field => [
-      field,
-      JSON.parse(
-        readFileSync(path.resolve(__dirname, `../../data/${field}.json`), { encoding: 'utf8' })
-      )
-    ])
+    .map(field => [field, JSON.parse(readFileSync(filePath(field), { encoding: 'utf8' }))])
     .reduce((hash, [name, value]) => {
       hash[name] = value;
       return hash;
     }, result);
 }
+
+export const filePath = (filename: string) => {
+  return path.resolve(__dirname, `../../data/${filename}.json`);
+};
---
+++ test/unit/lib/moderationList.test.ts
@@ -0,0 +1,38 @@
+import path from 'path';
+import * as testModerationList from '../../../src/lib/moderationList';
+
+describe('moderationList', () => {
+  it.each(['flaggedLinks', 'verifiedSpaces', 'flaggedProposalIds'])(
+    'returns only the %s',
+    field => {
+      const filePathSpy = jest
+        .spyOn(testModerationList, 'filePath')
+        .mockReturnValue(path.resolve(__dirname, `../../fixtures/${field}.json`));
+
+      const list = testModerationList.getModerationList([field]);
+      expect(list).toMatchSnapshot();
+      expect(filePathSpy).toHaveBeenCalledTimes(1);
+    }
+  );
+
+  it('returns multiple fields', () => {
+    const filePathSpy = jest
+      .spyOn(testModerationList, 'filePath')
+      .mockReturnValueOnce(path.resolve(__dirname, `../../fixtures/flaggedLinks.json`))
+      .mockReturnValueOnce(path.resolve(__dirname, `../../fixtures/verifiedSpaces.json`));
+
+    const list = testModerationList.getModerationList(['flaggedLinks', 'verifiedSpaces']);
+    expect(list).toMatchSnapshot();
+    expect(filePathSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it('ignores invalid fields', () => {
+    const filePathSpy = jest
+      .spyOn(testModerationList, 'filePath')
+      .mockReturnValue(path.resolve(__dirname, `../../fixtures/test.json`));
+
+    const list = testModerationList.getModerationList(['a', 'b']);
+    expect(list).toEqual({});
+    expect(filePathSpy).toHaveBeenCalledTimes(0);
+  });
+});
