885cc2f11696ed56354348758471bbfcd913e3cf
j0hnfl0w <ij69@pm.me>
2023-05-17 14:51:47 +0300

    init verifiedTokens

--- src/lib/moderationList.ts
+++ src/lib/moderationList.ts
@@ -1,7 +1,13 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 
-const FIELDS = ['flaggedLinks', 'flaggedProposals', 'verifiedSpaces', 'flaggedSpaces'];
+const FIELDS = [
+  'flaggedLinks',
+  'flaggedProposals',
+  'verifiedSpaces',
+  'flaggedSpaces',
+  'verifiedTokens'
+];
 const CACHE_PATH = path.resolve(__dirname, `../../${process.env.MODERATION_LIST_PATH || 'data'}`);
 
 export default function getModerationList(fields = FIELDS) {
