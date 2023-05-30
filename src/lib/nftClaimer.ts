ab8f96ff8fdc085cb65b4328de13bad5c8fa62cc
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-05-31 01:13:45 +0400

    fix: update mint signature validation

--- src/lib/nftClaimer.ts
+++ src/lib/nftClaimer.ts
@@ -2,16 +2,9 @@ import { getAddress } from '@ethersproject/address';
 import { Wallet } from '@ethersproject/wallet';
 import { splitSignature } from '@ethersproject/bytes';
 import snapshot from '@snapshot-labs/snapshot.js';
-import type { TypedDataField } from '@ethersproject/abstract-signer';
 import { fetchProposal, fetchSpace, Space } from '../helpers/snapshot';
 
-const domain = {
-  name: 'SpaceName',
-  version: '0.1',
-  chainId: '5' //TODO Use process.env.NETWORK once live
-};
-
-export const SpaceType = {
+const SpaceType = {
   Space: [
     { name: 'address', type: 'address' },
     { name: 'spaceId', type: 'string' },
@@ -19,22 +12,20 @@ export const SpaceType = {
   ]
 };
 
-export const MintType = {
-  Space: [
+const MintType = {
+  Mint: [
     { name: 'recipient', type: 'address' },
     { name: 'proposalId', type: 'uint256' },
     { name: 'salt', type: 'uint256' }
   ]
 };
 
-const NETWORK = process.env.NETWORK || '1';
+const HUB_NETWORK = process.env.NETWORK || '1';
+const NFT_CLAIMER_NETWORK = process.env.NFT_CLAIMER_NETWORK || '1';
 
 const signer = new Wallet(process.env.WALLET_PRIVATE_KEY as string);
 
-async function sign(message: Record<string, any>, type: Record<string, Array<TypedDataField>>) {
-  return splitSignature(await signer._signTypedData(domain, type, message));
-}
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function mintingAllowed(space: Space) {
   // TODO: Placeholder only for tests, remove once hub return nftClaimer results
   // https://github.com/snapshot-labs/snapshot-hub/pull/581
@@ -51,7 +42,7 @@ export async function signSpaceOwner(address: string, id: string, salt: number) 
     throw new Error('RECORD_NOT_FOUND');
   }
 
-  if ((await snapshot.utils.getSpaceController(id, NETWORK)) !== getAddress(address)) {
+  if ((await snapshot.utils.getSpaceController(id, HUB_NETWORK)) !== getAddress(address)) {
     throw new Error('Address is not the space owner');
   }
 
@@ -59,7 +50,17 @@ export async function signSpaceOwner(address: string, id: string, salt: number) 
     throw new Error('Space has not allowed minting');
   }
 
-  return sign({ address: getAddress(address), spaceId: id, salt }, SpaceType);
+  return splitSignature(
+    await signer._signTypedData(
+      {
+        name: 'SpaceName',
+        version: '0.1',
+        chainId: NFT_CLAIMER_NETWORK
+      },
+      SpaceType,
+      { address: getAddress(address), spaceId: id, salt }
+    )
+  );
 }
 
 export async function signValidProposal(address: string, id: string, salt: number) {
@@ -73,5 +74,15 @@ export async function signValidProposal(address: string, id: string, salt: numbe
     throw new Error('Space has not allowed minting');
   }
 
-  return sign({ recipient: getAddress(address), proposalId: id, salt }, MintType);
+  return splitSignature(
+    await signer._signTypedData(
+      {
+        name: 'NFT-CLAIMER',
+        version: '0.1',
+        chainId: NFT_CLAIMER_NETWORK
+      },
+      MintType,
+      { recipient: getAddress(address), proposalId: 100, salt }
+    )
+  );
 }
