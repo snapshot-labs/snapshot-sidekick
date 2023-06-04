7b4000bdcaa69df47a3beb673efa0fc528cec930
Wan <495709+wa0x6e@users.noreply.github.com>
2023-06-04 16:28:18 +0400

    fix: avoid duplicate processing of long running jobs (#48)

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -2,22 +2,32 @@ import { sleep, storageEngine } from '../helpers/utils';
 import VotesReport from './votesReport';
 
 export const queues = new Set<string>();
+const processingItems = new Set<string>();
 
 async function processItem(id: string) {
   console.log(`[queue] Processing queue item: ${id}`);
   try {
+    processingItems.add(id);
     await new VotesReport(id, storageEngine(process.env.VOTE_REPORT_SUBDIR)).generateCacheFile();
   } catch (e) {
     console.error(`[queue] Error while processing item`, e);
   } finally {
     queues.delete(id);
+    processingItems.delete(id);
   }
 }
 
 async function run() {
   try {
     console.log(`[queue] Poll queue (found ${queues.size} items)`);
-    queues.forEach(async item => processItem(item));
+    queues.forEach(async item => {
+      if (processingItems.has(item)) {
+        console.log(`[queue] Skip: ${item} is currently being processed`);
+        return;
+      }
+
+      processItem(item);
+    });
   } catch (e) {
     console.error(e);
   } finally {
