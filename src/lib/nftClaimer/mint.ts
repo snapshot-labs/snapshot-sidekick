5a3edb5d6df362dcf2d31c8d6f029b4d5aa9f1b4
Wan <495709+wa0x6e@users.noreply.github.com>
2023-07-04 11:23:43 +0900

    fix: fix nft claimer deploy signing (#73)
    * fix: update mint signature validation
    
    * fix: update domain name
    
    * chore: add nftClainer tests
    
    * chore: add more test
    
    * refactor: rename sign methods + improve tests
    
    * chore: skip yet untestable test
    
    * fix: fix overflow error on BigNumber
    
    * fix: only use test domain
    
    * chore: update test
    
    * Update .env.test
    
    * Update test/.env.test
    
    * chore: remove unused dependencies
    
    * chore: remove unused dependencies
    
    * fix: fix missing test env values
    
    * fix: update NFT Claimer deploy signer
    
    * fix: update signer
    
    * fix: update nft claimer deploy signature generation + refactoring
    
    * fix: remove unused import
    
    * chore: fix tests
    
    * fix: update deploy code for updated smart contract
    
    * fix: fix nft claimer deploy signature
    
    * fix: fix nftClaimer deploy signature
    
    * chore: fix tests
    
    * fix: remove unused env variable
    
    * fix: fix test for new deploy smart contract
    
    * fix: return also all params required for the tx
    
    * fix: add new proposer param to mint signature
    
    * fix: fix nft claimer mint signature generator
    
    * feat: retrieve mint contract address from subgraphj
    
    * fix: bump nft claimer subgraph version
    
    * fix: bump subgraph version
    
    * fix: update nft claimer subgraph url
    
    * feat: add support for base58 proposal ID
    
    * feat: sign message only if space has enabled minting
    
    * chore: fix tests
    
    * chore: fix tests
    
    * fix: return salt in deploy payload
    
    * fix: use number salt
    
    * fix: add support for both IPFS version for proposal ID
    
    * chore: fix merge conflict
    
    * chore: update README
    
    * fix: fix deploy signature
    
    * fix: fix signature
    
    * fix: check that spacecollection does not exist yet
    
    * feat: ensure that given address params are valid addresses
    
    * feat: validate address arguments
    
    * feat: use dynamic domain
    
    * fix: lock snapshot.js to 0.4.97
    
    Due to newer version breaking jest test
    
    * chore: fix timer issue on dependent snapshot.js
    
    * chore: enable jest fakeTimer globally
    
    * Revert "chore: enable jest fakeTimer globally"
    
    This reverts commit 524754dc536496c0ddc184f0bf7ec3ad3aa759ec.
    
    * Revert "chore: fix timer issue on dependent snapshot.js"
    
    This reverts commit ff2522f8d3af5cf64384168c1c30e3b5686b66b1.
    
    * feat: return `verifyingContract` in deploy payload
    
    * fix: use `forEach` for loop
    
    * fix: remove unused assertion
    
    * chore: print test code coverage
    
    * chore: move all process.env definition to file's top
    
    * fix: fail early to avoid unecessary request
    
    * fix: fail early to avoid unecessary request
    
    * chore: remove `NETWORK`, in favor of autodetection from `HUB_URL`
    
    * chore: remove unused check
    
    * chore: disable code coverage in e2e tests
    
    * chore: improve readability
    
    ---------
    
    Co-authored-by: Chaitanya <yourchaitu@gmail.com>

---
+++ src/lib/nftClaimer/mint.ts
@@ -0,0 +1,69 @@
+import { getAddress } from '@ethersproject/address';
+import { splitSignature } from '@ethersproject/bytes';
+import { fetchProposal } from '../../helpers/snapshot';
+import {
+  validateProposal,
+  getProposalContract,
+  signer,
+  numberizeProposalId,
+  validateAddresses
+} from './utils';
+
+const MintType = {
+  Mint: [
+    { name: 'proposer', type: 'address' },
+    { name: 'recipient', type: 'address' },
+    { name: 'proposalId', type: 'uint256' },
+    { name: 'salt', type: 'uint256' }
+  ]
+};
+
+const NFT_CLAIMER_NETWORK = process.env.NFT_CLAIMER_NETWORK;
+
+export default async function payload(
+  proposalAuthor: string,
+  recipient: string,
+  id: string,
+  salt: string
+) {
+  const proposal = await fetchProposal(id);
+  validateProposal(proposal, proposalAuthor);
+  validateAddresses({ proposalAuthor, recipient });
+  const spaceId = proposal?.space.id as string;
+
+  const verifyingContract = await getProposalContract(spaceId);
+
+  const message = {
+    proposer: getAddress(proposalAuthor),
+    recipient: getAddress(recipient),
+    proposalId: numberizeProposalId(id),
+    salt: BigInt(salt)
+  };
+
+  return {
+    signature: await generateSignature(verifyingContract, spaceId, message),
+    contractAddress: verifyingContract,
+    spaceId: proposal?.space.id,
+    ...message,
+    salt
+  };
+}
+
+async function generateSignature(
+  verifyingContract: string,
+  domain: string,
+  message: Record<string, string | bigint>
+) {
+  return splitSignature(
+    await signer._signTypedData(
+      {
+        name: domain,
+        version: '0.1',
+        chainId: NFT_CLAIMER_NETWORK,
+        verifyingContract
+      },
+      MintType,
+      message
+    )
+  );
+}
