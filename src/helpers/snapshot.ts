c7102467d61b68b86821bb3a60b06b523d81f205
Wan <495709+wa0x6e@users.noreply.github.com>
2023-07-24 12:21:19 +0900

    fix: fix fetchVotes progression not incrementing (#120)
    * fix: fix fetchVotes progression not incremented
    
    * fix: remove unused function
    
    * chore: fix test

--- src/helpers/snapshot.ts
+++ src/helpers/snapshot.ts
@@ -143,46 +143,6 @@ export async function fetchVotes(
   return votes;
 }
 
-export async function fetchAllVotes(id: string) {
-  let votes: Vote[] = [];
-  let page = 0;
-  let createdPivot = 0;
-  const pageSize = 1000;
-  let resultsSize = 0;
-  const maxPage = 5;
-
-  do {
-    let newVotes = await fetchVotes(id, {
-      first: pageSize,
-      skip: page * pageSize,
-      created_gte: createdPivot,
-      orderBy: 'created',
-      orderDirection: 'asc'
-    });
-    resultsSize = newVotes.length;
-
-    if (page === 0 && createdPivot > 0) {
-      // Loosely assuming that there will never be more than 1000 duplicates
-      const existingIpfs = votes.slice(-pageSize).map(vote => vote.ipfs);
-
-      newVotes = newVotes.filter(vote => {
-        return !existingIpfs.includes(vote.ipfs);
-      });
-    }
-
-    if (page === maxPage) {
-      page = 0;
-      createdPivot = newVotes[newVotes.length - 1].created;
-    } else {
-      page++;
-    }
-
-    votes = votes.concat(newVotes);
-  } while (resultsSize === pageSize);
-
-  return votes;
-}
-
 export async function fetchSpace(id: string) {
   const {
     data: { space }
