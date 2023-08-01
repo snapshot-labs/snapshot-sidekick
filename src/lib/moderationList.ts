3a00fbd2ccd623c44591e895c3e81d12fe726177
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-01 18:11:56 +0900

    feat: return new flagged items

--- src/lib/moderationList.ts
+++ src/lib/moderationList.ts
@@ -10,6 +10,9 @@ const FIELDS = new Map<keyof MODERATION_LIST, Record<string, string>>([
   ['flaggedProposals', { action: 'flag', type: 'proposal' }],
   ['flaggedSpaces', { action: 'flag', type: 'space' }],
   ['flaggedIps', { action: 'flag', type: 'ip' }],
+  ['flaggedAddresses', { action: 'flag', type: 'address' }],
+  ['flaggedProposalTitleKeyword', { action: 'flag', type: 'proposalTitleKeyword' }],
+  ['flaggedProposalBodyKeyword', { action: 'flag', type: 'proposalBodyKeyword' }],
   ['verifiedSpaces', { action: 'verify', type: 'space' }],
   ['verifiedTokens', { file: 'verifiedTokens.json' }]
 ]);
