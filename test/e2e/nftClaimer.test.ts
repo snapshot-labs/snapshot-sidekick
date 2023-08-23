d72726630692bd031244535cb71a11981ef0713f
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-24 00:23:31 +0900

    chore: retry flakky tests and add list of tests to implements

--- test/e2e/nftClaimer.test.ts
+++ test/e2e/nftClaimer.test.ts
@@ -4,6 +4,8 @@ const HOST = `http://localhost:${process.env.PORT || 3003}`;
 
 describe('nftClaimer', () => {
   describe('GET /api/nft-claimer', () => {
+    jest.retryTimes(3);
+
     it('returns an object with the snapshotFee', async () => {
       const response = await request(HOST).get('/api/nft-claimer');
       const fee = response.body.snapshotFee;
@@ -11,6 +13,27 @@ describe('nftClaimer', () => {
       expect(response.statusCode).toBe(200);
       expect(fee).toBeLessThan(100);
       expect(fee).toBeGreaterThanOrEqual(1);
+    });
+  });
+
+  describe('on deploy', () => {
+    describe('on valid input', () => {
+      it.todo('returns a payload');
+    });
+
+    describe('on invalid input', () => {
+      it.todo('returns a 401 error when the submitter is not the space controller');
+      it.todo('returns an error when the space does not exist');
+    });
+  });
+
+  describe('on mint', () => {
+    describe('on valid input', () => {
+      it.todo('returns a payload');
+    });
+
+    describe('on invalid input', () => {
+      it.todo('returns an error when proposal does not exist');
     });
   });
 });
