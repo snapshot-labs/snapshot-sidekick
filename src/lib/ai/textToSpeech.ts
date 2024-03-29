5511e2269a85b759512c23549684a6ac5dd8c7cd
Wan <495709+wa0x6e@users.noreply.github.com>
2024-03-29 18:57:13 +0400

    feat: AI text to speech for proposal body (#244)
    * feat: AI text to speech for proposal body
    
    * fix: add min proposal body length constraint
    
    * fix: return meaningful error message when proposal is not supported
    
    * fix: strip markdown before sending to speech AI
    
    * fix: fix error thrown when instantiating openAI with empty API KEY
    
    * fix: return a more precise error code
    
    * fix: fix invalid env var name
    
    ---------
    
    Co-authored-by: ChaituVR <yourchaitu@gmail.com>

---
+++ src/lib/ai/textToSpeech.ts
@@ -0,0 +1,50 @@
+import OpenAI from 'openai';
+import removeMd from 'remove-markdown';
+import Cache from '../cache';
+import { fetchProposal, Proposal } from '../../helpers/snapshot';
+import { IStorage } from '../storage/types';
+
+const MIN_BODY_LENGTH = 500;
+const MAX_BODY_LENGTH = 4096;
+
+export default class TextToSpeech extends Cache {
+  proposal?: Proposal | null;
+  openAi: OpenAI;
+
+  constructor(id: string, storage: IStorage) {
+    super(id, storage);
+    this.filename = `snapshot-proposal-ai-tts-${this.id}.mp3`;
+    this.openAi = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || 'Missing key' });
+  }
+
+  async isCacheable() {
+    this.proposal = await fetchProposal(this.id);
+
+    if (!this.proposal) {
+      throw new Error('RECORD_NOT_FOUND');
+    }
+
+    return true;
+  }
+
+  getContent = async () => {
+    this.isCacheable();
+    const body = removeMd(this.proposal!.body);
+
+    if (body.length < MIN_BODY_LENGTH || body.length > MAX_BODY_LENGTH) {
+      throw new Error('UNSUPPORTED_PROPOSAL');
+    }
+
+    try {
+      const mp3 = await this.openAi.audio.speech.create({
+        model: 'tts-1',
+        voice: 'alloy',
+        input: body
+      });
+
+      return Buffer.from(await mp3.arrayBuffer());
+    } catch (e: any) {
+      throw e.error?.code ? new Error(e.error?.code.toUpperCase()) : e;
+    }
+  };
+}
