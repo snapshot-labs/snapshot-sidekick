2537928c86beb593674931447cb5d8a389859b6e
Wan <495709+wa0x6e@users.noreply.github.com>
2025-09-05 05:49:33 +0400

    ci: update ubuntu version (#360)
    * ci: update ubuntu version
    
    * tests: fix test

--- test/integration/helpers/utils.test.ts
+++ test/integration/helpers/utils.test.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 
 describe('utils.ts', () => {
   describe('fetchWithKeepAlive', () => {
-    const TEST_URL = 'https://snapshot.org';
+    const TEST_URL = 'https://sh5.co';
 
     it('set the custom agent with keep-alive', async () => {
       const response = await fetchWithKeepAlive(TEST_URL);
